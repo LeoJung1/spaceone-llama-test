5c0ed39cc4ef11cae42824fc7a75512a0b71acdf
MZC01-JJUNG1 <mzc01-jjung1@MZC01-JJUNG1.local>
2023-11-15 14:32:44 +0900

    Renewed Llama Model

---
+++ source/finetune.py
@@ -0,0 +1,164 @@
+import os
+import sys
+import torch
+
+from datetime import datetime
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+    prepare_model_for_int8_training,
+    set_peft_model_state_dict,
+)
+from transformers import (AutoTokenizer, 
+                          AutoModelForCausalLM, 
+                          TrainingArguments, 
+                          Trainer, 
+                          DataCollatorForSeq2Seq
+)
+
+base_model = "codellama/CodeLlama-7b-hf"
+model = AutoModelForCausalLM.from_pretrained(
+    base_model,
+    load_in_8bit=True,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+tokenizer = AutoTokenizer.from_pretrained(base_model)
+
+eval_prompt = """You are a powerful text-to-SQL model. Your job is to answer questions about a database. You are given a question and context regarding one or more tables.
+
+You must output the SQL query that answers the question.
+### Input:
+Which Class has a Frequency MHz larger than 91.5, and a City of license of hyannis, nebraska?
+
+### Context:
+CREATE TABLE table_name_12 (class VARCHAR, frequency_mhz VARCHAR, city_of_license VARCHAR)
+
+### Response:
+"""
+# {'question': 'Name the comptroller for office of prohibition', 'context': 'CREATE TABLE table_22607062_1 (comptroller VARCHAR, ticket___office VARCHAR)', 'answer': 'SELECT comptroller FROM table_22607062_1 WHERE ticket___office = "Prohibition"'}
+model_input = tokenizer(eval_prompt, return_tensors="pt").to("cuda")
+
+model.eval()
+with torch.no_grad():
+    print(tokenizer.decode(model.generate(**model_input, max_new_tokens=100)[0], skip_special_tokens=True))
+
+
+tokenizer.add_eos_token = True
+tokenizer.pad_token_id = 0
+tokenizer.padding_side = "left"
+
+
+def tokenize(prompt):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=512,
+        padding=False,
+        return_tensors=None,
+    )
+
+    # "self-supervised learning" means the labels are also the inputs:
+    result["labels"] = result["input_ids"].copy()
+
+    return result
+
+def generate_and_tokenize_prompt(data_point):
+    full_prompt =f"""You are a powerful text-to-SQL model. Your job is to answer questions about a database. You are given a question and context regarding one or more tables.
+
+                You must output the SQL query that answers the question.
+
+                ### Input:
+                {data_point["question"]}
+
+                ### Context:
+                {data_point["context"]}
+
+                ### Response:
+                {data_point["answer"]}
+                """
+    return tokenize(full_prompt)
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt)
+
+model.train() # put model back into training mode
+model = prepare_model_for_int8_training(model)
+
+config = LoraConfig(
+    r=16,
+    lora_alpha=16,
+    target_modules=[
+    "q_proj",
+    "k_proj",
+    "v_proj",
+    "o_proj",
+],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+model = get_peft_model(model, config)
+
+wandb_project = "sql-try2-coder"
+if len(wandb_project) > 0:
+    os.environ["WANDB_PROJECT"] = wandb_project
+
+
+if torch.cuda.device_count() > 1:
+    # keeps Trainer from trying its own DataParallelism when more than 1 gpu is available
+    model.is_parallelizable = True
+    model.model_parallel = True
+
+
+batch_size = 128
+per_device_train_batch_size = 32
+gradient_accumulation_steps = batch_size // per_device_train_batch_size
+output_dir = "sql-code-llama"
+
+training_args = TrainingArguments(
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        warmup_steps=100,
+        max_steps=400,
+        learning_rate=3e-4,
+        fp16=True,
+        logging_steps=10,
+        optim="adamw_torch",
+        evaluation_strategy="steps", # if val_set_size > 0 else "no",
+        save_strategy="steps",
+        eval_steps=20,
+        save_steps=20,
+        output_dir=output_dir,
+        # save_total_limit=3,
+        load_best_model_at_end=False,
+        # ddp_find_unused_parameters=False if ddp else None,
+        group_by_length=True, # group sequences of roughly the same length together to speed up training
+        report_to="wandb", # if use_wandb else "none",
+        run_name=f"codellama-{datetime.now().strftime('%Y-%m-%d-%H-%M')}", # if use_wandb else None,
+    )
+
+trainer = Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=training_args,
+    data_collator=DataCollatorForSeq2Seq(
+        tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+    ),
+)
+
+model.config.use_cache = False
+
+old_state_dict = model.state_dict
+model.state_dict = (lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())).__get__(
+    model, type(model)
+)
+if torch.__version__ >= "2" and sys.platform != "win32":
+    print("compiling the model")
+    model = torch.compile(model)
+
+print("Training the Model...")
+trainer.train()
+print("Trainiing Done!")
--- source/preprocess/preprocess.py
+++ source/preprocess/preprocess.py
@@ -3,10 +3,14 @@ import json
 from datasets import load_dataset
 
 
-def preprocessing():
+
+# Out of Date
+def preprocessing() -> None:
     """
-    Preprocess the loaded dataset from huggingface and format them into a newly created JSON file.
+    Preprocess the loaded dataset from huggingface 
+    and format them into a newly created JSON file.
     """
+
     data = load_dataset("b-mc2/sql-create-context")
     training_dataset = {"train" : data["train"]}
 
@@ -15,12 +19,12 @@ def preprocessing():
         line_count = 0
         threshold = 100
         for row in training_dataset["train"]:
-            # if line_count == threshold:
-            #     break
+            if line_count == threshold:
+                break
             line = {"input" : row["question"],
                     "context" : row["context"],
                     "output" : row["answer"]}
             f.write(json.dumps(line) + "\n")
-            # line_count += 1
+            line_count += 1
 
 preprocessing()
